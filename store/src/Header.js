0de4820cb5f6d7af46a9af789c0a58dba5b977e7
Yana-Dyachok <annetart1994@gmail.com>
2023-11-21 20:13:02 +0200

    fix: change headers styles >

--- store/src/Header.js
+++ store/src/Header.js
@@ -1,43 +1,40 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import logo from '../src/assets/icons/logo.svg';
 const Header = () => {
-  return (
-    <header className="header">
-      <div className="header__nav">
-        <a href="#">
-        <img
-          className="logo"
-          src="../src/assets/icons/logo.svg"
-          alt="logo"
-          width="172"
-          height="87"
-        />
-        </a>
-        <nav className="nav">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <a className="nav__link" href="#">
-                Каталог
-              </a>
-            </li>
-            <li className="nav__item">
-              <a className="nav__link" href="#">
-                Акції
-              </a>
-            </li>
-            <li className="nav__item">
-              <a className="nav__link" href="#">
-                Служба підтримки
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="header__icons">
-          <div className="header__search"></div>
-          <div className="header__cart"></div>
-        </div>
-      </div>
-    </header>
-  );
+    return (
+        <header className="header">
+            <a href="#">
+                <img
+                    className="logo"
+                    src={logo}
+                    alt="logo"
+                    width="172"
+                    height="87"
+                />
+            </a>
+            <nav className="nav">
+                <ul className="nav__list">
+                    <li className="nav__item">
+                        <a className="nav__link" href="#">
+                            Каталог
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a className="nav__link" href="#">
+                            Акції
+                        </a>
+                    </li>
+                    <li className="nav__item">
+                        <a className="nav__link" href="#">
+                            Служба підтримки
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div className="header__search"></div>
+            <div className="header__cart"></div>
+        </header>
+    );
 };
 
 export default Header;

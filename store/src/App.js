a08cc505e7630d523dde8b9d7d7b3fc61bb6d961
Yana-Dyachok <annetart1994@gmail.com>
2023-11-21 20:35:35 +0200

    feat: add main, footer, hero section

--- store/src/App.js
+++ store/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Main/>
+        <Footer/>
       </div>
   );
 }
---
+++ store/src/Main.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Hero from './Hero';
+export class Main extends Component {
+  render() {
+    return (
+     <Hero/>
+    )
+  }
+}
+
+export default Main

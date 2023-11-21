575c6ea9aad8f03186aa76c0b4d37e3ae249d49c
Yana-Dyachok <annetart1994@gmail.com>
2023-11-21 19:57:03 +0200

    feat: add reset.scss and # to <a>

--- store/src/Header.js
+++ store/src/Header.js
@@ -3,10 +3,10 @@ const Header = () => {
   return (
     <header className="header">
       <div className="header__nav">
-        <a href="">
+        <a href="#">
         <img
           className="logo"
-          src="../src/assets/icons/logo.png"
+          src="../src/assets/icons/logo.svg"
           alt="logo"
           width="172"
           height="87"
@@ -15,25 +15,25 @@ const Header = () => {
         <nav className="nav">
           <ul className="nav__list">
             <li className="nav__item">
-              <a className="nav__link" href="">
+              <a className="nav__link" href="#">
                 Каталог
               </a>
             </li>
             <li className="nav__item">
-              <a className="nav__link" href="">
+              <a className="nav__link" href="#">
                 Акції
               </a>
             </li>
             <li className="nav__item">
-              <a className="nav__link" href="">
+              <a className="nav__link" href="#">
                 Служба підтримки
               </a>
             </li>
           </ul>
         </nav>
         <div className="header__icons">
-          <div className="search"></div>
-          <div className="shopping-cart"></div>
+          <div className="header__search"></div>
+          <div className="header__cart"></div>
         </div>
       </div>
     </header>

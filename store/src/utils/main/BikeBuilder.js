ec8716ba857a9346c7d1bee84f5e7ebb9a05f0a8
Yana-Dyachok <annetart1994@gmail.com>
2023-11-26 22:12:40 +0200

    feat: add radio btn

--- store/src/utils/main/BikeBuilder.js
+++ store/src/utils/main/BikeBuilder.js
@@ -1,22 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class BikeBuilder extends Component {
-  render() {
-    return (
-      <section id='bike-builder'>
-        <h2 className='bike-builder__title'>Підбір велосипеда</h2>
-        <div className='bike-builder__container'>
-            <div className='bike-builder__img'></div>
-            <div className='bike-builder__parameters'>
-                <h3 className='bike-builder__name'>Тип велосипеда</h3>
-                <h3 className='bike-builder__name'>Зріст</h3>
-                <h3 className='bike-builder__name'>Діаметр колес</h3>
-                <button className='bike-builder-btn btn' type='button'>Підібрати</button>
-            </div>
-        </div>
-      </section>
-    )
-  }
+    render() {
+        return (
+            <section id="bike-builder">
+                <h2 className="bike-builder__title">Підбір велосипеда</h2>
+                <div className="bike-builder__container">
+                    <div className="bike-builder__img"></div>
+                    <div className="bike-builder__parameters">
+                        <h3 className="bike-builder__name">Тип велосипеда</h3>
+                        <div className="bike-builder__buttons">
+                            <form>
+                              <input type="radio" id="bike-type-one" name="bike" value="bike-type" />
+                              <label className='btn' htmlFor="bike-type-one">Шосейний</label> 
+                              <input type="radio" id="bike-type-two" name="bike" value="bike-type" />
+                              <label  className='btn' htmlFor="bike-type-two"> Гірський</label> 
+                              <input type="radio" id="bike-type-three" name="bike" value="bike-type" />
+                              <label className='btn'  htmlFor="bike-type-three"> Міський</label> 
+                              <input type="radio" id="bike-type-four" name="bike" value="bike-type" />
+                              <label className='btn' htmlFor="bike-type-four"> Електро</label> 
+                            </form>
+                        </div>
+                        <h3 className="bike-builder__name">Зріст</h3>
+                        <h3 className="bike-builder__name">Діаметр колес</h3>
+                        <button className="bike-builder-btn btn" type="button">
+                            Підібрати
+                        </button>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
 
-export default BikeBuilder
+export default BikeBuilder;

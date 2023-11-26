0a81e705f0442271c283237a03c814ecf390a34a
Yana-Dyachok <annetart1994@gmail.com>
2023-11-26 21:13:16 +0200

    fix: rename classes

--- store/src/utils/footer/Footer.js
+++ store/src/utils/footer/Footer.js
@@ -6,43 +6,41 @@ import telegram from '../../../src/assets/icons/telegram.svg';
 export class Footer extends Component {
   render() {
     return (
-        // може id для footer?
       <footer className='footer'> 
         <div className='footer__information'>
-            <div className='information__item bike-info '>
-                <h3 className='bike-info__title'>Велосипеди</h3>
-                <ul className='bike-info__list'> 
-                    <li className='bike-info__item' ><a className='bike-info__link' href="">Види велосипедів</a></li>
-                    <li className='bike-info__item'><a className='bike-info__link'  href=""></a>Каталог</li>
-                    <li className='bike-info__item'><a className='bike-info__link' href="">Акції</a></li>
-                    <li className='bike-info__item'><a className='bike-info__link' href=""></a>Пошук</li>
+            <div className='footer__information__item'>
+                <h3 className='footer__information__name'>Велосипеди</h3>
+                <ul className='footer__information__list'> 
+                    <li className='footer__information__item' ><a className='footer__information__link' href="">Види велосипедів</a></li>
+                    <li className='footer__information__item'><a className='footer__information__link'  href=""></a>Каталог</li>
+                    <li className='footer__information__item'><a className='footer__information__link' href="">Акції</a></li>
+                    <li className='footer__information__item'><a className='footer__information__link' href=""></a>Пошук</li>
                 </ul>
             </div>
-            <div className='information__item about-info'>
-                <h3 className='about-info__title'>Інформація</h3>
-                <ul className='about-info__list'> 
-                    <li className='about-info__item' ><a className='about-info__link' href="">Про нас</a></li>
-                    <li className='about-info__item'><a className='about-info__link'  href=""></a>Контактиг</li>
+            <div className='footer__information__item'>
+                <h3 className='footer__information__name'>Інформація</h3>
+                <ul className='footer__information__list'> 
+                    <li className='footer__information__item' ><a className='footer__information__link' href="">Про нас</a></li>
+                    <li className='footer__information__item'><a className='footer__information__link'  href=""></a>Контактиг</li>
                 </ul>
             </div>
-            <div className='information__item customers-info'>
-                <h3 className='customers-info__title'>Клієнтам</h3>
-                <ul className='customers-info__list'> 
-                    <li className='customersinfo__item' ><a className='customers-info__link' href="">Доставка</a></li>
-                    <li className='customers-info__item'><a className='customers-info__link'  href=""></a>Умови оплати</li>
-                    <li className='customers-info__item'><a className='customers-info__link' href="">Гарантія</a></li>
-                    <li className='customers-info__item'><a className='customers-info__link' href=""></a>Повернення</li>
+            <div className='footer__information__item'>
+                <h3 className='footer__information__name'>Клієнтам</h3>
+                <ul className='footer__information__list'> 
+                    <li className='footer__information__item' ><a className='footer__information__link' href="">Доставка</a></li>
+                    <li className='footer__information__item'><a className='footer__information__link'  href=""></a>Умови оплати</li>
+                    <li className='footer__information__item'><a className='footer__information__link' href="">Гарантія</a></li>
+                    <li className='footer__information__item'><a className='footer__information__link' href=""></a>Повернення</li>
                 </ul>
             </div>
         </div>
         <div className='footer__contacts'>
-            {/* не зрозуміло що це, може ©2023 BraveWheel? */}
-            <a href="#">@2023 BraveWheel</a> 
-            <ul className='contacts__list'>
-                <li  className='contacts__item'><a  className='contacts__link mail-icon' href="#"><img src={mail} alt="mail" width='30' height='30'/></a></li>
-                <li  className='contacts__item'><a className='contacts__link facebook-icon' href="#"><img src={facebook} alt="facebook" width='30' height='30'/></a></li>
-                <li  className='contacts__item'><a className='contacts__link instagram-icon' href="#"><img src={instagram} alt="instagram" width='30' height='30'/></a></li>
-                <li  className='contacts__item'><a className='contacts__link telegram-icon'href="#"><img src={telegram} alt="telegram" width='30' height='30'/></a></li>
+            <a href="#">&copy;2023 BraveWheel</a> 
+            <ul className='footer__contacts__list'>
+                <li  className='footer__contacts__item'><a  className='footer__contacts__link' href="#"><img src={mail} alt="mail" width='30' height='30'/></a></li>
+                <li  className='footer__contacts__item'><a className='footer__contacts__link' href="#"><img src={facebook} alt="facebook" width='30' height='30'/></a></li>
+                <li  className='footer__contacts__item'><a className='footer__contacts__link' href="#"><img src={instagram} alt="instagram" width='30' height='30'/></a></li>
+                <li  className='footer__contacts__item'><a className='footer__contacts__link'href="#"><img src={telegram} alt="telegram" width='30' height='30'/></a></li>
             </ul>
         </div>
       </footer>

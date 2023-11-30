3b46e90cb804db6ffb12a463d6bddfa2ce8a8a64
Yana-Dyachok <annetart1994@gmail.com>
2023-11-30 12:07:12 +0200

    fix: fix hero section

--- store/src/utils/main/Hero.jsx
+++ store/src/utils/main/Hero.jsx
@@ -6,15 +6,12 @@ export class Hero extends Component {
             <section className="hero">
                 <div className="container">
                     <div className="hero__text">
-                        <h1 className="hero__title">Розкрий свій шлях</h1>
-                        <h2 className="hero__subtitle">
+                        <h1 className="hero__name titles">Розкрий свій шлях</h1>
+                        <h2 className="hero__description">
                             Підбери велосипед, який створенний для твоїх
                             досягнень!
                         </h2>
-                        <button
-                            className="hero__btn btn"
-                            type="button"
-                        >
+                        <button className="hero__btn btn" type="button">
                             Підібрати
                         </button>
                     </div>
